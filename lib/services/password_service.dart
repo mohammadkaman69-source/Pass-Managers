4335691c0f5b6f7ea64d3bb3f95331ee951e5207
iranzamin2012 <soheill.2012@gmail.com>
2026-08-07 23:30:43 +0330

    Update password_service.dart

--- lib/services/password_service.dart
+++ lib/services/password_service.dart
@@ -2,31 +2,59 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class PasswordService {
 
-  static const String key = "master_password";
+  static const String passwordKey = "master_password";
+  static const String emailKey = "recovery_email";
+
 
   static Future<bool> hasPassword() async {
-    final prefs = await SharedPreferences.getInstance();
-
-    return prefs.containsKey(key);
-  }
-
-
-  static Future<void> savePassword(String password) async {
 
     final prefs = await SharedPreferences.getInstance();
 
-    await prefs.setString(key, password);
+    return prefs.containsKey(passwordKey);
 
   }
 
 
-  static Future<bool> checkPassword(String password) async {
+  static Future<void> saveCredentials(
+    String password,
+    String email,
+  ) async {
 
     final prefs = await SharedPreferences.getInstance();
 
-    final saved = prefs.getString(key);
+    await prefs.setString(
+      passwordKey,
+      password,
+    );
 
-    return saved == password;
+    await prefs.setString(
+      emailKey,
+      email,
+    );
+
+  }
+
+
+  static Future<bool> checkPassword(
+    String password,
+  ) async {
+
+    final prefs = await SharedPreferences.getInstance();
+
+    final savedPassword =
+        prefs.getString(passwordKey);
+
+
+    return savedPassword == password;
+
+  }
+
+
+  static Future<String?> getRecoveryEmail() async {
+
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(emailKey);
 
   }
 
