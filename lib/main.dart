31f4c41538defa1f7894e87fb590a163708ecf7e
iranzamin2012 <soheill.2012@gmail.com>
2026-08-10 01:00:47 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,26 @@
 import 'package:flutter/material.dart';
-import 'screens/login_page.dart';
 
+import 'screen/login_page.dart';
 
-void main(){
-
-runApp(
-const PassManagers()
-);
-
+void main() {
+  runApp(
+    const PassManagers(),
+  );
 }
 
+class PassManagers extends StatelessWidget {
+  const PassManagers({super.key});
 
-
-class PassManagers extends StatelessWidget{
-
-const PassManagers({super.key});
-
-
-@override
-Widget build(BuildContext context){
-
-
-return MaterialApp(
-
-debugShowCheckedModeBanner:false,
-
-title:"Pass Managers",
-
-theme:ThemeData(
-
-colorSchemeSeed:Colors.blue,
-
-useMaterial3:true,
-
-),
-
-
-home:const LoginPage(),
-
-
-);
-
-}
-
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pass Managers',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
 }
