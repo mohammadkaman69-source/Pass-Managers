ff3c6b8fb163fdfbabfb8cc57c25aab01467e505
iranzamin2012 <soheill.2012@gmail.com>
2026-08-11 14:55:14 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,71 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home_page.dart';
 import 'screens/login_page.dart';
 import 'security/app_lifecycle_manager.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final lifecycleManager =
-      AppLifecycleManager();
-
-  lifecycleManager.start();
-
   runApp(
     const PassManagers(),
   );
 }
 
-class PassManagers extends StatelessWidget {
+class PassManagers extends StatefulWidget {
   const PassManagers({
     super.key,
   });
+
+  @override
+  State<PassManagers> createState() =>
+      _PassManagersState();
+}
+
+class _PassManagersState
+    extends State<PassManagers> {
+  late final AppLifecycleManager
+      _lifecycleManager;
+
+  bool _isLocked = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lifecycleManager =
+        AppLifecycleManager(
+      onLock: _lockApp,
+    );
+
+    _lifecycleManager.start();
+  }
+
+  @override
+  void dispose() {
+    _lifecycleManager.stop();
+    super.dispose();
+  }
+
+  void _lockApp() {
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _isLocked = true;
+    });
+  }
+
+  void _unlockApp() {
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _isLocked = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +76,11 @@ class PassManagers extends StatelessWidget {
         colorSchemeSeed: Colors.blue,
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: _isLocked
+          ? LoginPage(
+              onLoginSuccess: _unlockApp,
+            )
+          : const HomePage(),
     );
   }
 }
