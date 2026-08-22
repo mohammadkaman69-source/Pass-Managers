9afa2ae3d5f29e156f95e7d0bee9f2882793b5c9
mohammadkaman69-source <mohammad.kaman69@gmail.com>
2026-08-22 16:10:06 +0330

    Allow crypto KDF salt as generic byte list

--- lib/security/crypto_service.dart
+++ lib/security/crypto_service.dart
@@ -8,21 +8,17 @@ class CryptoService {
   CryptoService();
 
   final AesGcm _aes = AesGcm.with256bits();
-
   final Random _random = Random.secure();
 
   Uint8List generateSalt([int length = 32]) {
     return Uint8List.fromList(
-      List<int>.generate(
-        length,
-        (_) => _random.nextInt(256),
-      ),
+      List<int>.generate(length, (_) => _random.nextInt(256)),
     );
   }
 
   Future<SecretKey> deriveKey({
     required String masterPassword,
-    required Uint8List salt,
+    required List<int> salt,
   }) async {
     final algorithm = Argon2id(
       memory: 32 * 1024,
@@ -32,9 +28,7 @@ class CryptoService {
     );
 
     return algorithm.deriveKey(
-      secretKey: SecretKey(
-        utf8.encode(masterPassword),
-      ),
+      secretKey: SecretKey(utf8.encode(masterPassword)),
       nonce: salt,
     );
   }
@@ -44,7 +38,6 @@ class CryptoService {
     required SecretKey key,
   }) async {
     final nonce = _aes.newNonce();
-
     final secretBox = await _aes.encrypt(
       utf8.encode(plainText),
       secretKey: key,
@@ -57,37 +50,22 @@ class CryptoService {
       'mac': base64Encode(secretBox.mac.bytes),
     };
 
-    return base64Encode(
-      utf8.encode(
-        jsonEncode(result),
-      ),
-    );
+    return base64Encode(utf8.encode(jsonEncode(result)));
   }
 
   Future<String> decrypt({
     required String encryptedText,
     required SecretKey key,
   }) async {
-    final decoded = utf8.decode(
-      base64Decode(encryptedText),
-    );
-
-    final data =
-        jsonDecode(decoded) as Map<String, dynamic>;
-
+    final decoded = utf8.decode(base64Decode(encryptedText));
+    final data = jsonDecode(decoded) as Map<String, dynamic>;
     final secretBox = SecretBox(
       base64Decode(data['cipherText'] as String),
       nonce: base64Decode(data['nonce'] as String),
-      mac: Mac(
-        base64Decode(data['mac'] as String),
-      ),
+      mac: Mac(base64Decode(data['mac'] as String)),
     );
 
-    final clearText = await _aes.decrypt(
-      secretBox,
-      secretKey: key,
-    );
-
+    final clearText = await _aes.decrypt(secretBox, secretKey: key);
     return utf8.decode(clearText);
   }
 }
